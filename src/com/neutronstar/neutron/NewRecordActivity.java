33fa90ed4a205acda76c787b19be3870f7fd83ad
admin <admin@LENOVO-F238C26F>
2013-12-11 20:22:55 +0800

    test

--- src/com/neutronstar/neutron/NewRecordActivity.java
+++ src/com/neutronstar/neutron/NewRecordActivity.java
@@ -184,7 +184,7 @@ public class NewRecordActivity extends Activity {
 		dateTitle.setText(titleList[3]);
 		dateValue.setHint(titleList[4]);
 		
-	}
+	} 
 	
 	public void save(View v){
 		SQLiteDatabase db = ndb.getWritableDatabase();
