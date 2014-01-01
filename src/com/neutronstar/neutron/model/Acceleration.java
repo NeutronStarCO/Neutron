f54784468ce1aff1113fe2a50367d5882dc43485
Tong Zhao <zhaotong1984@gmail.com>
2014-01-01 23:55:56 +0800

    Acceleration Upload Method

---
+++ src/com/neutronstar/neutron/model/Acceleration.java
@@ -0,0 +1,34 @@
+package com.neutronstar.neutron.model;
+
+import java.io.Serializable;
+
+public class Acceleration implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4572703078330071684L;
+	private double acceleration;
+	private String timestamp;
+	private int uploadtag;
+	
+	public Acceleration() {}
+	
+	public Acceleration(double acceleration, String timestamp, int uploadtag)
+	{
+		super();
+		this.acceleration = acceleration;
+		this.timestamp = timestamp;
+		this.uploadtag = uploadtag;
+	}
+	
+	public int getUploadtag() {return this.uploadtag;}
+	public void setUploadtag(int uploadtag) {this.uploadtag = uploadtag;}
+	
+	public double getAcceleration() {return this.acceleration; }
+	public void setAcceleration(double acceleration) { this.acceleration = acceleration; }
+	
+	public String getTimestamp() {return this.timestamp;}
+	public void setTimestamp(String timestamp) {this.timestamp = timestamp;}
+	
+}
