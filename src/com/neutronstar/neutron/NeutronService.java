8f6f29e5fc4b9f40ae8e16f8d5cf6b538de97819
Tong Zhao <zhaotong1984@gmail.com>
2014-01-03 00:10:27 +0800

    doInBackground 中的 isSucceed不能改为result。

--- src/com/neutronstar/neutron/NeutronService.java
+++ src/com/neutronstar/neutron/NeutronService.java
@@ -13,6 +13,7 @@ import java.security.SecureRandom;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.Timer;
@@ -187,7 +188,6 @@ public class NeutronService extends Service {
 				return;
 			}
 		}
-
 		// 第二步 将数据上传至服务器
 		String strUrl = SERVER.PublicAddress + "/" + strServlet;
 		ConnectivityManager connMgr = (ConnectivityManager) getSystemService(this.CONNECTIVITY_SERVICE);
@@ -260,12 +260,6 @@ public class NeutronService extends Service {
 				Toast toast = Toast.makeText(NeutronService.this,
 						"Upload Acceleration Failed.", Toast.LENGTH_LONG);
 				toast.show();
-				
-				NeutronDbHelper ndb = NeutronDbHelper.GetInstance(NeutronService.this);
-				SQLiteDatabase db = ndb.getWritableDatabase();
-				ContentValues cv = new ContentValues();
-				cv.put("uploadtag",1);
-				db.update(NeutronAcceleration.TABLE_NAME, cv, "uploadtag=?", new String[]{"0"});  
 			}
 		}
 	}
