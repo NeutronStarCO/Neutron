43622e1028353c5b43b1bc42d6ababb563a4c56d
Tong Zhao <zhaotong1984@gmail.com>
2014-01-02 23:51:37 +0800

    Upload Acceleration 网服务器发送数据功能已完成。

--- src/com/neutronstar/neutron/NeutronService.java
+++ src/com/neutronstar/neutron/NeutronService.java
@@ -10,8 +10,10 @@ import java.io.Serializable;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.security.SecureRandom;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.Timer;
@@ -36,6 +38,7 @@ import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.neutron.server.persistence.model.T_accdata;
 import com.neutron.server.persistence.model.T_user;
 import com.neutronstar.neutron.NeutronContract.NeutronAcceleration;
 import com.neutronstar.neutron.NeutronContract.NeutronUser;
@@ -53,8 +56,10 @@ public class NeutronService extends Service {
 	private Timer updateTimer;
 	private double lowAcc;
 	private final double FILTERING_VALUE = 0.8;
-	
-	T_user localUser = null; 
+
+	T_user localUser = null;
+	T_accdata accData = null;
+	ArrayList<Serializable> alAccData = null;
 
 	public NeutronService() {
 		// TODO Auto-generated constructor stub
@@ -81,7 +86,7 @@ public class NeutronService extends Service {
 		sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 		accelerometer = sensorManager
 				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-		
+
 		localUser = getLocalUser();
 		updateTimer = new Timer("gForceUpdateService");
 		updateTimer.scheduleAtFixedRate(new TimerTask() {
@@ -89,17 +94,18 @@ public class NeutronService extends Service {
 				refreshAccelerometer();
 			}
 		}, 0, 5000);
-		
+
 		SecureRandom random = new SecureRandom();
-		int d1 = random.nextInt(10) * 1000;   
+		int d1 = random.nextInt(10) * 1000;
+		alAccData = new ArrayList<Serializable>();
 		updateTimer.scheduleAtFixedRate(new TimerTask() {
 			@Override
 			public void run() {
 				// TODO Auto-generated method stub
-				uploadAcceleration();
+				uploadAcceleration("data", alAccData);
 				// 先测试一分钟上传一次的情况
 			}
-		}, d1, 60000);
+		}, 0, 60000);
 	}
 
 	@Override
@@ -120,214 +126,177 @@ public class NeutronService extends Service {
 		NeutronDbHelper ndb = NeutronDbHelper.GetInstance(this);
 		SQLiteDatabase db = ndb.getWritableDatabase();
 		ContentValues cv = new ContentValues();
-		cv.put(NeutronAcceleration.COLUMN_NAME_ACCELERATION, currentAcceleration);
+		cv.put(NeutronAcceleration.COLUMN_NAME_ACCELERATION,
+				currentAcceleration);
 		cv.put(NeutronAcceleration.COLUMN_NAME_TIMESTAMP, date);
 		cv.put(NeutronAcceleration.COLUMN_NAME_UPLOADTAG, 0);
 		db.insert(NeutronAcceleration.TABLE_NAME, null, cv);
 
 	}
-	
-	private void uploadAcceleration() {
-		
+
+	private void uploadAcceleration(String strServlet,
+			ArrayList<Serializable> alAccData) {
+
 		// 第一步 从数据库中取到 UPLOADTAG 为0的值（未上传的数据）
 		NeutronDbHelper ndb = NeutronDbHelper.GetInstance(this);
 		SQLiteDatabase db = ndb.getReadableDatabase();
-		String[] projection = {
-			    NeutronAcceleration.COLUMN_NAME_ACCELERATION,
-				NeutronAcceleration.COLUMN_NAME_TIMESTAMP
-			    };
-		String selection = "" + NeutronAcceleration.COLUMN_NAME_UPLOADTAG + "=" + 0;
-		Cursor cur = db.query(
-				NeutronAcceleration.TABLE_NAME,  // The table to query
-			    projection,                // The columns to return
-			    selection,                 // The columns for the WHERE clause selection
-			    null,                      // The values for the WHERE clause selectionArgs
-			    null,                      // don't group the rows
-			    null,                      // don't filter by row groups
-			    null                  	// The sort order
-			    );
-		
-		
-		ArrayList<Serializable> paraList = new ArrayList<Serializable>();
-		Acceleration acc = null;
-		if (cur != null) 
-		{
-			if (cur.moveToFirst())
-			{
-				do 
-				{
-					acc = new Acceleration();
-					acc.setAcceleration(cur.getDouble(cur.getColumnIndex(NeutronAcceleration.COLUMN_NAME_ACCELERATION)));
-					acc.setTimestamp(cur.getString(cur.getColumnIndex(NeutronAcceleration.COLUMN_NAME_TIMESTAMP)));
-					paraList.add(acc);
-				}while (cur.moveToNext());
-			}
-			else
-			{
-				acc = null;
+		String[] projection = { NeutronAcceleration.COLUMN_NAME_ACCELERATION,
+				NeutronAcceleration.COLUMN_NAME_TIMESTAMP };
+		String selection = "" + NeutronAcceleration.COLUMN_NAME_UPLOADTAG + "="
+				+ 0;
+		Cursor cur = db.query(NeutronAcceleration.TABLE_NAME, // The table to
+																// query
+				projection, // The columns to return
+				selection, // The columns for the WHERE clause selection
+				null, // The values for the WHERE clause selectionArgs
+				null, // don't group the rows
+				null, // don't filter by row groups
+				null // The sort order
+				);
+
+		accData = null;
+		if (cur != null) {
+			if (cur.moveToFirst()) {
+				do {
+					accData = new T_accdata();
+					accData.settAccdataUserid(localUser.gettUserId());
+					accData.settAccdataValue(cur.getDouble(cur
+							.getColumnIndex(NeutronAcceleration.COLUMN_NAME_ACCELERATION)));
+
+					String strDate = cur
+							.getString(cur
+									.getColumnIndex(NeutronAcceleration.COLUMN_NAME_TIMESTAMP));
+					Date dt = new Date();
+
+					SimpleDateFormat sDateFormat = new SimpleDateFormat(
+							"yyyy-MM-dd HH:mm:ss.SSS");
+					try {
+						dt = sDateFormat.parse(strDate);
+					} catch (ParseException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+					accData.settAccdataDatatime(dt);
+					alAccData.add(accData);
+				} while (cur.moveToNext());
+			} else {
+				accData = null;
+				Toast toast = Toast.makeText(this,
+						"Everything up to date.", Toast.LENGTH_LONG);
+				toast.show();
+				return;
 			}
 		}
-		String acce = "";
-		Iterator iterator = paraList.iterator();
-		acc = null;
-		while(iterator.hasNext())
-		{
-			acc = (Acceleration)iterator.next();
-			acce += acc.getAcceleration() + acc.getTimestamp();
+		// 第二步 将数据上传至服务器
+		String strUrl = SERVER.PublicAddress + "/" + strServlet;
+		ConnectivityManager connMgr = (ConnectivityManager) getSystemService(this.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+		if (networkInfo != null && networkInfo.isConnected()) {
+			new UploadAccelerationTask().execute(strUrl);
+		} else {
+			Toast toast = Toast.makeText(this,
+					"No network connection available.", Toast.LENGTH_LONG);
+			toast.show();
 		}
-		// 第二步  将数据上传至服务器
-		int id = localUser.gettUserId();
-		sendAcceleration("", id, acce);
-		
 	}
 
-	private void sendAcceleration(String strServlet, int id, String acce)
-	{
-		String strUrl = SERVER.Address + "/" + strServlet;	
-        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(this.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-        if (networkInfo != null && networkInfo.isConnected()) {
-            new SendAccelerationTask().execute(strUrl, String.valueOf(id), acce);
-        } else {
-        	Toast toast = Toast.makeText(this, "No network connection available.", Toast.LENGTH_LONG );
-			toast.show();
-        }
-	}
-	
-	 private class SendAccelerationTask extends AsyncTask<String, Void, String> 
-	 {
+	private class UploadAccelerationTask extends
+			AsyncTask<String, Void, String> {
 
 		@Override
 		protected String doInBackground(String... params) {
 			String strUrl = params[0];
-			int id = Integer.valueOf(params[1]);
-			String acce = params[2];
 			try {
-				 URL url = new URL(strUrl);
-			     HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
-			     urlConn.setReadTimeout(10000 /* milliseconds */);
-			     urlConn.setConnectTimeout(15000 /* milliseconds */);
-			     urlConn.setDoInput(true);
-			     urlConn.setDoOutput(true);
-			     urlConn.setRequestMethod("POST");
-			     urlConn.setUseCaches(false);
-				urlConn.setRequestProperty("Content-Type", "application/x-java-serialized-object");
+				URL url = new URL(strUrl);
+				HttpURLConnection urlConn = (HttpURLConnection) url
+						.openConnection();
+				urlConn.setReadTimeout(10000 /* milliseconds */);
+				urlConn.setConnectTimeout(15000 /* milliseconds */);
+				urlConn.setDoInput(true);
+				urlConn.setDoOutput(true);
+				urlConn.setRequestMethod("POST");
+				urlConn.setUseCaches(false);
+				urlConn.setRequestProperty("Content-Type",
+						"application/x-java-serialized-object");
 				urlConn.connect();
-				OutputStream outStrm = urlConn.getOutputStream();  
-		        ObjectOutputStream oos = new ObjectOutputStream(outStrm);  
-		        
-		        ArrayList<Serializable> paraList = new ArrayList<Serializable>();
-		        paraList.add("sendacceleration");
-		        paraList.add(id);
-		        paraList.add(acce);
-		        oos.writeObject(paraList);  
-		        oos.flush();  
-		        oos.close();  
-		  
-		        ObjectInputStream ois = new ObjectInputStream(urlConn.getInputStream());  
-		        paraList = (ArrayList<Serializable>)ois.readObject();
-		        String isSucceed = (String)paraList.get(0);
-		        Log.d("isSucceed", isSucceed);
+				OutputStream outStrm = urlConn.getOutputStream();
+				ObjectOutputStream oos = new ObjectOutputStream(outStrm);
 
-            } catch (Exception e) {
-                return "Unable to send acceleration to the server. The server may be invalid.";
-            }
-			return "isSucceed";
+				ArrayList<Serializable> paraList = new ArrayList<Serializable>();
+				paraList.add("upload");
+				paraList.add(alAccData);
+				oos.writeObject(paraList);
+				oos.flush();
+				oos.close();
+
+				ObjectInputStream ois = new ObjectInputStream(
+						urlConn.getInputStream());
+				paraList = (ArrayList<Serializable>) ois.readObject();
+				String isSucceed = (String) paraList.get(0);
+				Log.d("ok", isSucceed);
+
+			} catch (Exception e) {
+				Log.d("error", "error");
+				return "error";
+			}
+			return "ok";
 		}
-		 
-		protected void onPostExecute(String result) 
-		{
-			if(result == "isSucceed")
-			{
-				Toast toast = Toast.makeText(NeutronService.this, "Upload Acceleration Succeed.", Toast.LENGTH_LONG );
+
+		protected void onPostExecute(String result) {
+			if (result == "ok") {
+				NeutronDbHelper ndb = NeutronDbHelper
+						.GetInstance(NeutronService.this);
+				SQLiteDatabase db = ndb.getWritableDatabase();
+				ContentValues cv = new ContentValues();
+				cv.put("uploadtag", 1);
+				String[] args = { "0" };
+				db.update(NeutronAcceleration.TABLE_NAME, cv, "uploadtag=?",
+						args);
+				Toast toast = Toast.makeText(NeutronService.this,
+						"Upload Acceleration Succeed.", Toast.LENGTH_LONG);
+				toast.show();
+			} else {
+				Toast toast = Toast.makeText(NeutronService.this,
+						"Upload Acceleration Failed.", Toast.LENGTH_LONG);
 				toast.show();
 			}
-			else
-			{
-				Toast toast = Toast.makeText(NeutronService.this, "Upload Acceleration Failed.", Toast.LENGTH_LONG );
-				toast.show();
-			}
-			
-//			if(null != remoteUser)
-//			{
-				// 已存在用户登录成功，转入主页面
-//				if(localUser.gettUserId().equals(remoteUser.gettUserId())
-//						&& localUser.gettUserPasscode().equals(remoteUser.gettUserPasscode()))
-//				{
-//					
-//				}
-//					new Handler().postDelayed(new Runnable() {
-//						public void run() {
-//							Intent intent = new Intent(Appstart.this, MainNeutron.class);
-//							Bundle bl = new Bundle();
-//							bl.putInt("id", localUser.gettUserId());
-//							intent.putExtras(bl);
-//							startActivity(intent);
-//							Appstart.this.finish();
-//						}
-//					}, 1000);
-//				else 
-//				{
-//					
-//				}
-				//转入登录注册页面1
-//					new Handler().postDelayed(new Runnable() {
-//						public void run() {
-//							Intent intent = new Intent(Appstart.this, Welcome.class);
-//							startActivity(intent);
-//							Appstart.this.finish();
-//						}
-//					}, 1000);
-//			} else 
-//			{
-//				
-//			}
-			//转入登录注册页面1
-//				new Handler().postDelayed(new Runnable() {
-//					public void run() {
-//						Intent intent = new Intent(Appstart.this, Welcome.class);
-//						startActivity(intent);
-//						Appstart.this.finish();
-//					}
-//				}, 1000);
 		}
-	 }
-	
-	 
-	 private T_user getLocalUser()
-		{
-			T_user user = null;
-			NeutronDbHelper ndb = NeutronDbHelper.GetInstance(this);
-			SQLiteDatabase db = ndb.getReadableDatabase();
-			String[] projection = {
-				    NeutronUser.COLUMN_NAME_ID,
-				    NeutronUser.COLUMN_NAME_PASSCODE
-				    };
-			String selection = "" + NeutronUser.COLUMN_NAME_RELATION + "=" + USER.me
-					+ " AND " + NeutronUser.COLUMN_NAME_TAG + "=" + TAG.normal;
-			Cursor cur = db.query(
-					NeutronUser.TABLE_NAME,  // The table to query
-				    projection,                // The columns to return
-				    selection,                 // The columns for the WHERE clause selection
-				    null,                      // The values for the WHERE clause selectionArgs
-				    null,                      // don't group the rows
-				    null,                      // don't filter by row groups
-				    null                  		// The sort order
-				    );
-			if (cur != null) {
-				if (cur.moveToFirst()) {
-					user = new T_user();
-					do {
-						user.settUserId(cur.getInt(cur.getColumnIndex(NeutronUser.COLUMN_NAME_ID)));
-						user.settUserPasscode(cur.getString(cur.getColumnIndex(NeutronUser.COLUMN_NAME_PASSCODE)));
-					} while (cur.moveToNext());
-				}else{
-					user = null;
-				}
+	}
+
+	private T_user getLocalUser() {
+		T_user user = null;
+		NeutronDbHelper ndb = NeutronDbHelper.GetInstance(this);
+		SQLiteDatabase db = ndb.getReadableDatabase();
+		String[] projection = { NeutronUser.COLUMN_NAME_ID,
+				NeutronUser.COLUMN_NAME_PASSCODE };
+		String selection = "" + NeutronUser.COLUMN_NAME_RELATION + "="
+				+ USER.me + " AND " + NeutronUser.COLUMN_NAME_TAG + "="
+				+ TAG.normal;
+		Cursor cur = db.query(NeutronUser.TABLE_NAME, // The table to query
+				projection, // The columns to return
+				selection, // The columns for the WHERE clause selection
+				null, // The values for the WHERE clause selectionArgs
+				null, // don't group the rows
+				null, // don't filter by row groups
+				null // The sort order
+				);
+		if (cur != null) {
+			if (cur.moveToFirst()) {
+				user = new T_user();
+				do {
+					user.settUserId(cur.getInt(cur
+							.getColumnIndex(NeutronUser.COLUMN_NAME_ID)));
+					user.settUserPasscode(cur.getString(cur
+							.getColumnIndex(NeutronUser.COLUMN_NAME_PASSCODE)));
+				} while (cur.moveToNext());
+			} else {
+				user = null;
 			}
-			return user;
 		}
-	
+		return user;
+	}
+
 	private final SensorEventListener sensorEventListener = new SensorEventListener() {
 		// 系统设置的重力加速度标准值，设备在水平静止的情况下就承受这个压力，所以默认Y轴方向的加速度值为STANDARD_GRAVITY
 		double calibration = SensorManager.STANDARD_GRAVITY;
@@ -344,11 +313,12 @@ public class NeutronService extends Service {
 			double a = Math.round(Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)
 					+ Math.pow(z, 2)));
 			double acc = Math.abs((float) (a - calibration));
-			lowAcc = lowAcc * FILTERING_VALUE + acc * ( 1.0f - FILTERING_VALUE );
+			lowAcc = lowAcc * FILTERING_VALUE + acc * (1.0f - FILTERING_VALUE);
 
 			// 消去原有的重力引起的压力
 			currentAcceleration = Math.abs(acc - lowAcc);
-			Log.i("sensor", "\n Service currentAcceleration " + currentAcceleration);
+			Log.i("sensor", "\n Service currentAcceleration "
+					+ currentAcceleration);
 			sensorManager.unregisterListener(sensorEventListener);
 		}
 	};
